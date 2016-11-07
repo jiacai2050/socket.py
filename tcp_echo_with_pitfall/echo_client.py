ff6dcfa9275088dbe8e5b087b0db502241712a9f
jiacai2050 <jiacai2050@gmail.com>
2016-11-07 20:16:19 +0800

    add safe_socket.py

--- tcp_echo_with_pitfall/echo_client.py
+++ tcp_echo_with_pitfall/echo_client.py
@@ -1,23 +1,20 @@
 # coding=utf8
+
 import socket
+from safe_socket import safe_send, safe_recv
+
+data_to_sent = 'hello tcp socket'
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print sock.getsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF)
-    data_to_sent = 'hello tcp socket'
+
     try:
         sock.connect(('', 5600))
 
-        total_send = 0
-        content_length = len(data_to_sent)
-        while total_send < content_length:
-            sent = sock.send(data_to_sent[total_send:])
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            total_send += total_send + sent
+        safe_send(sock, data_to_sent)
 
-        print(sock.recv(1024))
+        # echo server 返回相应的大小与发送的一致
+        MSGLEN = len(data_to_sent)
+        print(safe_recv(sock, MSGLEN))
     finally:
         sock.close()
-        print('socket closed')
-
--- tcp_echo_with_pitfall/echo_server.py
+++ tcp_echo_with_pitfall/echo_server.py
@@ -1,18 +1,22 @@
 # coding=utf8
+
 import socket
+from safe_socket import safe_send, safe_recv
+# 服务器需要知道客户端发的数据包的大小，这样才能知道收到的数据是否完整
+# http 协议里面是通过 Content-Length 头部获取的
+from echo_client import data_to_sent
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+MSGLEN = len(data_to_sent)
 
 def handler(client_sock, addr):
     try:
         print('new client from %s:%s' % addr)
-        msg = client_sock.recv(100)
-        client_sock.send(msg)
+        msg = safe_recv(client_sock, MSGLEN)
         print('received data[%s] from %s:%s' % ((msg,) + addr))
+        safe_send(client_sock, msg)
     finally:
         client_sock.close()
-        print('client[%s:%s] socket closed' % addr)
 
 if __name__ == '__main__':
 
